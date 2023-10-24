47acdb51aa3668837f2c8b8bf559c74204a5e87d
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-10-24 18:26:19 +0200

    Update ejercicio1.py

--- clase 1/ejercicio1.py
+++ clase 1/ejercicio1.py
@@ -17,22 +17,39 @@ def f(x, y):
 
 # Lo pongo en practica para resolver la ecuación diferencial y' = (2 - x - y) / (x - y + 4), no tenemos datos sobre h y n asi que me los invento 
 
-x0 = -2 
-y0 = 0  
-h = 0.1 
-n = 100 
+n= int(input("Ingrese el valor de n: "))
+
+x0 = int(input("Ingrese el valor de x0: "))
+y0 = int(input("Ingrese el valor de y0: ")) 
+h = ( - x0)/n
+
+x1 = int(input("Ingrese el valor de x1: "))
+y1 = int(input("Ingrese el valor de y1: "))
+h1 = ( - x1)/n
+
+x2 = int(input("Ingrese el valor de x2: "))
+y2 = int(input("Ingrese el valor de y2: "))
+h2 = ( - x2)/n
 
 # Resolver la EDO usando el método de Euler
 
 x_values, y_values = euler_method(f, x0, y0, h, n)
+x1_values, y1_values = euler_method(f, x1, y1, h1, n)
+x2_values, y2_values = euler_method(f, x2, y2, h2, n)
 
 # Imprimir los resultados
 
 for x, y in zip(x_values, y_values):
     print(f'x = {x:.2f}, y = {y:.6f}')
 
+for x, y in zip(x1_values, y1_values):
+    print(f'x = {x:.2f}, y = {y:.6f}')
+
+for x, y in zip(x2_values, y2_values):
+    print(f'x = {x:.2f}, y = {y:.6f}')
+
 # Graficar la solución
 
 import matplotlib.pyplot as plt
-plt.plot(x_values, y_values)
+plt.plot(x_values, y_values, x1_values, y1_values, x2_values, y2_values)
 plt.show()
